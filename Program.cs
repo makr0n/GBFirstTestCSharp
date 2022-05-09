4d81b0a96121cf39294d990e855ae9ce0dc5be4f
SergeiPospelov <pospelka413@gmail.com>
2022-05-09 17:27:19 +0300

    Edit comments

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
-﻿//Вводим массив строк
-
+﻿
 Console.Write("Введите текст через пробел и нажмите Enter: ");
-string inputText = Convert.ToString(Console.ReadLine()); // ввод текста пользователем
-string[] arrayInput = inputText.Split(' '); // создаем массив из введенного текста
+string inputText = Convert.ToString(Console.ReadLine()); // Ввод текста пользователем
+string[] arrayInput = inputText.Split(' '); // Создаем массив из введенного текста
 
 
 string[] GetArrayWithThreeSymbolsStrings(string[] array)
 {
     int wordCount = 0;
-    for (int i = 0; i < array.Length; i++) // цикл для нахождения количества слов, удовлетворяющих условию задачи
+    for (int i = 0; i < array.Length; i++) // Цикл для нахождения количества слов, удовлетворяющих условию задачи
     {
         if (array[i].Length <= 3)
         {
@@ -17,9 +16,9 @@ string[] GetArrayWithThreeSymbolsStrings(string[] array)
     }
 
     int count = 0;
-    string[] threeSymbolsArray = new string[wordCount];// создаем массив длиной равной количеству слов, удовлетворяющих условию задачи
+    string[] threeSymbolsArray = new string[wordCount];// Создаем массив длиной равной количеству слов, удовлетворяющих условию задачи
 
-    for (int i = 0; i < array.Length; i++) // записываем слова состоящие из 3 или менее символов в новый массив.
+    for (int i = 0; i < array.Length; i++) // Записываем слова состоящие из 3 или менее символов в новый массив.
     {
         if (array[i].Length <= 3)
         {
@@ -31,7 +30,7 @@ string[] GetArrayWithThreeSymbolsStrings(string[] array)
     return threeSymbolsArray;
 }
 
-void PrintArray(string[] array)
+void PrintArray(string[] array)// Метод для вывода массива на экран
 {
     int count = array.Length;
     Console.Write("[");
